913ac7c5c1369b686d3d3802b2a1fc2ee00aa9e4
palolegovich <Retho@yandex.ru>
2024-04-01 23:03:37 +0300

    add typescript, delete babel

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,30 +8,20 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
     mode: 'development',
-    devtool: 'source-map',
+    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin(
             {template: path.resolve(process.cwd(), 'src', 'index.html')}
         )
     ],
-    entry: path.resolve(process.cwd(), 'src', 'index.js'),
-    output: {
-        path: path.resolve(process.cwd(), 'dist'),
-        clean: true,
-    },
+    entry: path.resolve(process.cwd(), 'src', 'index.tsx'),
     module: {
         rules: [
             { test: /\.(html)$/, use: ['html-loader'] }, // Добавляем загрузчик для html
             {
-                test: /\.js$/,
+                test: /\.tsx$/,
                 exclude: /node_modules/, // не обрабатываем файлы из node_modules
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        cacheDirectory: true, // Использование кэша для избежания рекомпиляции
-                        // при каждом запуске
-                    },
-                },
+                use: 'ts-loader'
             },
             {
                 test: /\.jsx?$/, // обновляем регулярное выражение для поддержки jsx
@@ -44,6 +34,13 @@ module.exports = {
                 },
             },
         ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(process.cwd(), 'dist'),
+        clean: true,
     },
     devServer: {
         hot: true,
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 
+const getTestString = (): string => {return 'd'}
 const root = createRoot(document.getElementById('root-div'));
-root.render(<h1>Hello, world</h1>);
+root.render(<h1>Hello, {getTestString()}</h1>);
