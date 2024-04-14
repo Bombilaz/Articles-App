a01151aaa67ec7bdcbfaab22cf49df63b439a1f3
palolegovich <Retho@yandex.ru>
2024-04-14 16:52:07 +0300

    add App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-
-const getTestString = (): string => {return 'привет'}
-
+import { App } from "./App.";
 
 const root = createRoot(document.getElementById('root-div'));
-root.render(<h1>Hello, {getTestString()}</h1>);
+root.render(<App />);
