e59f86af04a53a4338ba1b662359d4279a333a93
palolegovich <Retho@yandex.ru>
2024-04-16 00:34:08 +0300

    add routing + base styles header

--- src/App..tsx
+++ src/App..tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import {Header} from "./components/Header";
 import './index.scss';
+import { Route, Routes } from "react-router-dom";
 
 export const App = () => {
     return (
         <>
             <Header />
-            <div className='test'>Hellk World</div>
+            <Routes>
+                <Route path='/' element={<div>all articles</div>} />
+                <Route path='/my-articles' element={<div>my articles</div>} />
+                <Route path='/add-article' element={<div>add article</div>} />
+                <Route path='/profile' element={<div>profile</div>} />
+            </Routes>
         </>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
 import {FunctionComponent} from "react";
 import classes from './Header.module.scss'
+import {NavLink} from "react-router-dom";
 
 export const Header: FunctionComponent = () => {
     console.log(classes)
     return (
-        <div className={classes.testModule}>
-            <span>my arctiles</span>
-            <span>all articles</span>
-            <span>profile</span>
-            <span>add article</span>
-        </div>
+        <header className={classes.wrapper}>
+            <h1>Mambet Pages</h1>
+
+            <nav className={classes.nav}>
+                <NavLink to='/'>all articles</NavLink>
+                <NavLink to='/my-articles'>my articles</NavLink>
+                <NavLink to='/add-article'>add article</NavLink>
+                <NavLink to='/profile'>profile</NavLink>
+            </nav>
+        </header>
     )
 }
